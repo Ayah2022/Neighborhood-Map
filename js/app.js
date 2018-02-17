89b5d4746028f28337cee84ac3aa7725331651b1
Ayah2022 <aya.rateb2022@yahoo.com>
2018-02-17 14:58:17 +0200

    exchange files

--- js/app.js
+++ js/app.js
@@ -1,31 +1,28 @@
 // JavaScript Document
-
 var map;
 var marker;
 var currentMarker = null;
-var markers= [];
+var markers = [];
 
 
 function initMap() {
-	  //create map
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {
-                    lat: 30.088746,
-                    lng: 31.312691
-                },
-                zoom: 16
-            });
-			// Style the markers a bit. This will be our listing marker icon.
-var defaultIcon = makeMarkerIcon('0091ff');
-// Create a "highlighted location" marker color for when the user
-// mouses over the marker.
-var highlightedIcon = makeMarkerIcon('FFFF24');
+    //create map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 30.088746,
+            lng: 31.312691
+        },
+        zoom: 16
+    });
+    // Style the markers a bit. This will be our listing marker icon.
+    var defaultIcon = makeMarkerIcon('0091ff');
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('FFFF24');
 
-var bounds = new google.maps.LatLngBounds();
-
-			
-	//Iterate over the restarants and create a marker for each one
-             restaurants.forEach(function(restaurant) {
+    var bounds = new google.maps.LatLngBounds();
+    //Iterate over the restarants and create a marker for each one
+    restaurants.forEach(function(restaurant) {
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(restaurant.coordinates),
             animation: google.maps.Animation.DROP,
@@ -36,39 +33,40 @@ var bounds = new google.maps.LatLngBounds();
             mLng: restaurant.coordinates.lng
 
         });
- //put the markers
-                markers.push(marker);
-                // Two event listeners - one for mouseover, one for mouseout,
-                // to change the colors back and forth.
-                marker.addListener('mouseover', function() {
-                    this.setIcon(highlightedIcon);
-                });
-                marker.addListener('mouseout', function() {
-                    this.setIcon(defaultIcon);
-                });		
-            marker.addListener('click', (function(marker) {
-            return function () {
+        //put the markers
+        markers.push(marker);
+        // Two event listeners - one for mouseover, one for mouseout,
+        // to change the colors back and forth.
+        marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        });
+        marker.addListener('click', (function(marker) {
+            return function() {
                 getMarkerInfo(marker);
                 /*Prevents more than one marker from being animated at a time*/
                 if (marker) {
-				marker.setAnimation(null);
-                currentMarker = marker;
-                currentMarker.setAnimation(google.maps.Animation.BOUNCE);
-                setTimeout(function() {
-                    currentMarker.setAnimation(null);
-                }, 1500);
-				}
-           };
-       })(marker));	
- // Extend the boundaries of the map for each marker and display the marker
-            for ( i = 0; i < markers.length; i++) {
-                markers[i].setMap(map);
-                bounds.extend(markers[i].position);
-            }
-            map.fitBounds(bounds);
-	   
-  });
- function makeMarkerIcon(markerColor) {
+                    marker.setAnimation(null);
+                    currentMarker = marker;
+                    currentMarker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function() {
+                        currentMarker.setAnimation(null);
+                    }, 1500);
+                }
+            };
+        })(marker));
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+            bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+
+    });
+
+    function makeMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
             'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
             '|40|_|%E2%80%A2',
@@ -78,14 +76,13 @@ var bounds = new google.maps.LatLngBounds();
             new google.maps.Size(21, 34));
         return markerImage;
     }
- 
-			
-            //CALL THE viewmodel
-            ko.applyBindings(new viewModel());
- 
-}
-function HandleError(){
-    alert('Invalid Map');
+
+
+    //CALL THE viewmodel
+    ko.applyBindings(new viewModel());
+
 }
 
- 
+function HandleError() {
+    alert('Invalid Map');
+}
--- js/model.js
+++ js/model.js
@@ -1,40 +1,40 @@
 // JavaScript Document
 var viewModel = function() {
-	var self= this;
-	var infowindow = new google.maps.InfoWindow();
-	//observable for storing and updating the input
-	self.restaurantInput = ko.observable('');
-	//observable array for copying markers
-	self.restaurantMarkers = ko.observableArray();
-	//creating copy of markers
-	for (var i=0; i< markers.length; i++){
-		self.restaurantMarkers.push(markers[i]);
-	}
-	//animating the marker and opening the info window
-	self.listedRest = function(marker){
-		google.maps.event.trigger(marker, 'click');
-	}
-	
-	
-	//function to filter markers
-	self.filteredRest = ko.computed(function() {
-		//lowercasing ands storing the input
-		var filter = self.restaurantInput().toLowerCase();
-		//checking the matching of input entered to the markers
-		//all markers stay visible if there is no match
-		if(!filter) {
-			self.restaurantMarkers().forEach(function (item){
-				item.setVisible(true);
-			});
-			return self.restaurantMarkers();
-			//handling input if not matched with knockout arrayfilter method
-		} else{
-			return ko.utils.arrayFilter(self.restaurantMarkers(),function(item){
-				//store,lowercase marker name ,make sure it is >=0 and set visibility for matched ones
-				var match = item.name.toLowerCase().indexOf(filter) >= 0;
-				item.setVisible(match);
-				return match;
-			});
-		}
-	},self);
+    var self = this;
+    var infowindow = new google.maps.InfoWindow();
+    //observable for storing and updating the input
+    self.restaurantInput = ko.observable('');
+    //observable array for copying markers
+    self.restaurantMarkers = ko.observableArray();
+    //creating copy of markers
+    for (var i = 0; i < markers.length; i++) {
+        self.restaurantMarkers.push(markers[i]);
+    }
+    //animating the marker and opening the info window
+    self.listedRest = function(marker) {
+        google.maps.event.trigger(marker, 'click');
+    }
+
+
+    //function to filter markers
+    self.filteredRest = ko.computed(function() {
+        //lowercasing ands storing the input
+        var filter = self.restaurantInput().toLowerCase();
+        //checking the matching of input entered to the markers
+        //all markers stay visible if there is no match
+        if (!filter) {
+            self.restaurantMarkers().forEach(function(item) {
+                item.setVisible(true);
+            });
+            return self.restaurantMarkers();
+            //handling input if not matched with knockout arrayfilter method
+        } else {
+            return ko.utils.arrayFilter(self.restaurantMarkers(), function(item) {
+                //store,lowercase marker name ,make sure it is >=0 and set visibility for matched ones
+                var match = item.name.toLowerCase().indexOf(filter) >= 0;
+                item.setVisible(match);
+                return match;
+            });
+        }
+    }, self);
 }
